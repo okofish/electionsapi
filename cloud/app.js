efe6235a0ee83de5f71101928fd840b2a3741f97
Jesse Friedman <jesse@jesse.ws>
2015-07-19 00:34:10 -0400

    Adding data type list on api

--- cloud/app.js
+++ cloud/app.js
@@ -28,11 +28,19 @@ app.get('/elections/:year/:election', function(req, res) {
   query.equalTo("year", req.params.year);
   query.find({
     success: function(results) {
+      var dataTypes = [];
+      _.map(["popularVote", "electoralStateVote"], function(dtype) {
+        if (results[0].get(dtype) != undefined) {
+          dataTypes.push(dtype.toLowerCase())
+        }
+      })
       res.send({
         "year": results[0].get("year"),
         "type": results[0].get("type"),
         "totalVotes": results[0].get("totalVotes"),
-        "votingAge": results[0].get("votingAge")
+        "votingAge": results[0].get("votingAge"),
+        "dataTypes": dataTypes,
+        "url": req.path
       })
     },
     error: function(error) {
@@ -51,7 +59,8 @@ app.get('/elections/:year/:election/data/popularvote', function(req, res) {
       res.send({
         "year": results[0].get("year"),
         "type": results[0].get("type"),
-        "popularVote": results[0].get("popularVote")
+        "popularVote": results[0].get("popularVote"),
+        "url": req.path
       })
     },
     error: function(error) {
@@ -70,7 +79,8 @@ app.get('/elections/:year/:election/data/electoralstatevote', function(req, res)
       res.send({
         "year": results[0].get("year"),
         "type": results[0].get("type"),
-        "electoralStateVote": results[0].get("electoralStateVote")
+        "electoralStateVote": results[0].get("electoralStateVote"),
+        "url": req.path
       })
     },
     error: function(error) {
@@ -79,23 +89,43 @@ app.get('/elections/:year/:election/data/electoralstatevote', function(req, res)
   });
 });
 
+app.get('/elections', function(req, res) {
+  var Election = Parse.Object.extend("Election");
+  var query = new Parse.Query(Election);
+  query.find({
+    success: function(results) {
+      var electionslist = [];
+      for (election in results) {
+        electionslist.push({
+          "year": results[election].get("year"),
+          "type": results[election].get("type")
+        })
+      }
+      res.send({
+        "elections": electionslist,
+        "url": req.path
+      })
+    },
+    error: function(error) {
+      console.error("Error: " + error.code + " " + error.message);
+    }
+  });
+})
+
 app.get('/', function(req, res) {
   res.send({
     "elections_url": "/elections"
   })
 })
 
-// // Example reading from the request query string of an HTTP get request.
-// app.get('/test', function(req, res) {
-//   // GET http://example.parseapp.com/test?message=hello
-//   res.send(req.query.message);
-// });
-
-// // Example reading from the request body of an HTTP post request.
-// app.post('/test', function(req, res) {
-//   // POST http://example.parseapp.com/test (with request body "message=hello")
-//   res.send(req.body.message);
-// });
+app.use(function(req, res) {
+  res.send({
+    error: {
+      code: 404,
+      message: 'Endpoint not found.'
+    }
+  });
+});
 
 // Attach the Express app to Cloud Code.
 app.listen();
